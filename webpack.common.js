f667e2e199be5d8988c1fc3f2e8971f04cb279e4
ivalynx <inkybasik@gmail.com>
2025-10-13 11:46:12 +0300

    First version of site

---
+++ webpack.common.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'docs'),
+    assetModuleFilename: pathData => {
+      const filepath = path.dirname(pathData.filename).split('/').slice(1).join('/');
+      return `${filepath}/[name][ext]`;
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /\.module\.s[ac]ss$/i,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { modules: false },
+          },
+          { 
+            loader: 'sass-loader',
+            options: { implementation: require('sass') },
+          },
+        ],
+      },
+
+      // 2. Правило для ЛОКАЛЬНЫХ стилей (Используют модули)
+      {
+        test: /\.module\.(c|sa|sc)ss$/i,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { modules: true },
+          },
+          { 
+            loader: 'sass-loader',
+            options: { implementation: require('sass') },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { modules: false },
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
+      },
+      {
+        test: /\.svg$/,
+        type: 'asset',
+      },
+      {
+        test: /\.(png|ico|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/img", to: "img" },
+        // { from: "src/favicon", to: "" },
+        // { from: "src/files", to: "files" },
+      ],
+    }),
+  ],
+};
